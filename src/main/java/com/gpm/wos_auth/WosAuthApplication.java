1ae897be8546d73a4b062abdaa66c8bfe0324449
GPM <javiergenepaul@gmail.com>
2026-04-21 16:24:00 +0800

    first commit

---
+++ src/main/java/com/gpm/wos_auth/WosAuthApplication.java
@@ -0,0 +1,13 @@
+package com.gpm.wos_auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WosAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WosAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gpm/wos_auth/WosAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gpm.wos_auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WosAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
